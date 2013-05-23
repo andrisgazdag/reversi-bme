48c567fbaba3e10e58cb55a55b9fb2c5f7dd0a33
Győri Lajos Zsombor <satraf.ckar@gmail.com>
2013-05-23 13:18:35 +0200

    AI

--- src/Reversi/AI.java
+++ src/Reversi/AI.java
@@ -22,28 +22,31 @@ public class AI {
     }
             
     public int[] step() {
-        int[][] optimals = new int[3][2];
+        int optimals[][] = new int[3][2];
         int max1 = 0, max2 = 0, max3 = 0;
         int size = game.getTableSize().getSize();
         for (int jj = 0; jj < size; ++jj) {
             for (int ii = 0; ii < size; ++ii) {
+                int[] azistenbasszonmeg = {jj, ii};
                 int score = ctrlr.isStepValid(jj, ii, false)[0];
-                if ( score > max1) {
+                if (score > max1) {
                     max3 = max2;
                     max2 = max1;
                     max1 = score;
-                    optimals[3] = {jj,ii};
-                    
-                    
+                    optimals[2] = optimals[1];
+                    optimals[1] = optimals[0];
+                    optimals[0] = azistenbasszonmeg;
+                } else if (score > max2) {
+                    max3 = max2;
+                    max2 = score;
+                    optimals[2] = optimals[1];
+                    optimals[1] = azistenbasszonmeg;
+                } else if (score > max3) {
+                    max3 = score;
+                    optimals[2] = azistenbasszonmeg;
                 }
-                else if (score > max2) {
-                }
-                else if (score > max3) {
-                }
-
             }
         }
-
         return optimals[level.getLevel()];
     }
 }
